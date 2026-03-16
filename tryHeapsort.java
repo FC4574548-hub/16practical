7cd278a51d60e59a53af06285ceee864a97c2129
FC4574548-hub <4574548@myuwc.ac.za>
2026-03-16 14:50:45 +0200

    Update tryHeapsort.java

--- tryHeapsort.java
+++ tryHeapsort.java
@@ -3,42 +3,56 @@ import java.util.*;
 
 public class tryHeapsort {
     static String[] load(String filename) throws Exception {
-        
+
         ArrayList<String> words = new ArrayList<>();
         Scanner s = new Scanner(new File(filename));
-        
+
         while (s.hasNext()){
             String w = s.next();
             w = w.replaceAll("[^a-zA-Z]", "").toLowerCase();
             if (!w.isEmpty())
                 words.add(w);
 
-        }  
+        }
         s.close();;
         return words.toArray(new String[0]);
-        
+
     }
-    static void heapify(String[] arr, int size, int i){
+    static void heapify(String[] arr, int n, int i){
         int smallest = i;
         int left = 2*i + 1;
         int right = 2*i + 2;
 
-        if (left < size && arr[left].compareTo(arr[smallest]) < 0)
+        if (left < n && arr[left].compareTo(arr[smallest]) < 0)
             smallest = left;
-        if(right < size && arr[right].compareTo(arr[smallest]) < 0)
+        if(right < n && arr[right].compareTo(arr[smallest]) < 0)
             smallest = right;
         if(smallest != i){
             String temp = arr[i];
             arr[i] = arr[smallest];
             arr[smallest] = temp;
-            
-            heapify(arr,size, smallest);
+
+            heapify(arr,n, smallest);
         }
-            
+
     }
     /* Build bottom up*/
-    static void buildBottomUp(String[] heap, int index){
+    static void buildBottomUp(String[] arr){
+        for (int i = arr.length/2 - 1; i >= 0; i++)
+            heapify(arr, arr.length, i);
         
+
     }
+    public static void insert(String[] arr, int index){
+        int parent = (index - 1)/2;
+        
+        while (index > 0 && arr[index].compareTo(arr[parent]) < 0) {
+            String temp = arr[index];
+            arr[index] = arr[parent];
+            arr[parent] = temp;
+            index = parent;
+            parent = (index - 1)/2;
+        }
     }
+    
 }
